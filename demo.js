d5f53f50a4c3e8b078d50b61f9f43e9833775424
aman <amanagrawal581@gmail.com>
2023-01-23 20:58:40 +0530

    added more htmls

---
+++ demo.js
@@ -0,0 +1,7 @@
+var headerTitle = document.getElementById('header-title')
+//var title = document.getElementsByClassName('title')
+
+headerTitle.style.borderBottom = 'solid 3px black'
+const title = document.getElementById('xyz')
+title.style.color = "green"
+title.style.fontWeight = "bold"
