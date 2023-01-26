711d508b112164abb12bb4cd29a764b26ab18008
aman <amanagrawal581@gmail.com>
2023-01-26 15:46:00 +0530

    initial commit

--- demo.js
+++ demo.js
@@ -5,3 +5,14 @@ headerTitle.style.borderBottom = 'solid 3px black'
 const title = document.getElementById('xyz')
 title.style.color = "green"
 title.style.fontWeight = "bold"
+console.log(title.textContent)
+
+const items = document.getElementsByClassName('list-group-item')
+//console.log(items)
+//console.log(items[]);
+items[2].style.backgroundColor = 'green';
+for (i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold'
+    items[i].style.color = 'red'
+}
+//listItems[1].style.color  = "green"
